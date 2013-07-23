c82052c2189cd18d2cd98b2291812bd8d383db03
Chifeng Chou <cfchou@gmail.com>
2013-07-23 10:49:26 +0100

    FSM drink serving

--- src/main/scala/FlightAttendant.scala
+++ src/main/scala/FlightAttendant.scala
@@ -5,11 +5,17 @@
  */
 package zzz.akka.avionics
 
-import akka.actor.Actor
+import akka.actor.{Cancellable, ActorRef, Actor}
 
 object FlightAttendant {
   case class GetDrink(drinkkname: String)
   case class Drink(drinkname: String)
+
+  case class Assist(passenger: ActorRef)
+  case object Busy_?
+  case object Yes
+  case object No
+
   def apply() = new FlightAttendant with AttendantResponsiveness {
     val maxResponseTimeMS: Int = 300000
   }
@@ -23,14 +29,54 @@ class FlightAttendant extends Actor {
 
   implicit val ec = context.dispatcher // for scheduler
 
-  def receive: Actor.Receive = {
-    case GetDrink(drinkname) =>
-      // schedule to serve
-      // def scheduleOnce(FiniteDuration, ActorRef, Any)
-      // even sender is a function, sender is frozen at call site(call-by-value).
-      context.system.scheduler.scheduleOnce(responseDuration, sender,
-        Drink(drinkname))
+  // internal msg signals that a delivery can take place
+  case class DeliverDrink(drinkname: String)
+  // stores scheduleDelivery, could be canceled
+  var pendingDelivery: Option[Cancellable] = None
+
+  def scheduleDelivery(drinkname: String): Cancellable = {
+    context.system.scheduler.scheduleOnce(responseDuration, self,
+      DeliverDrink(drinkname))
   }
+
+  def assistInjuredPassenger: Receive = {
+    case Assist(passenger) =>
+      pendingDelivery map { _.cancel() }
+      pendingDelivery = None
+      passenger ! Drink("Magic")
+  }
+
+  def handleDrinkRequests: Receive = {
+    case GetDrink(drinkname) =>
+      pendingDelivery = Some(scheduleDelivery(drinkname))
+      // orElse is short-circut
+      context.become(assistInjuredPassenger orElse handleSpecificPerson(sender))
+    case Busy_? =>
+      sender ! No
+  }
+
+  // we're serving someone
+  def handleSpecificPerson(person: ActorRef): Receive = {
+    // the person we're serving has a new request
+    case GetDrink(drinkname) if sender == person =>
+      pendingDelivery map { _.cancel() }
+      pendingDelivery = Some(scheduleDelivery(drinkname))
+
+    // ScheduleDelivery signals us to deliver drink now
+    case DeliverDrink(drinkname) =>
+      person ! drinkname
+      pendingDelivery = None
+      context.become(assistInjuredPassenger orElse handleDrinkRequests)
+
+    // got request from another person, forward to the lead attendant
+    case m: GetDrink =>
+      context.parent forward(m)
+
+    case Busy_? =>
+      sender ! Yes
+  }
+
+  def receive = assistInjuredPassenger orElse handleDrinkRequests
 }
 
 
